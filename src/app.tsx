d023cb8ef70c45e97127edc23efc506d499dd65e
TamaraRibeiro <tamara.fl@hotmail.com>
2025-04-20 23:51:44 -0300

    :hammer: improve root css

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,8 @@ import ModalLogin from "./components/modal-login";
 export default function App() {
 
   return (
-    <div className="font-roboto antialiased bg-grey-500 h-screen grid place-items-center px-6">
+    <>
       <ModalLogin />
-    </div>
+    </>
   )
 }
