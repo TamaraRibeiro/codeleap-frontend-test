6ceabe22094ad41039584d2938c5591c4f3db4d8
TamaraRibeiro <tamara.fl@hotmail.com>
2025-04-20 23:42:46 -0300

    :sparkles: save user in localStorage

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,10 +1,18 @@
+import { useNavigate } from "react-router";
 import { useUser } from "../contexts/userContext"
 
 export default function Home() {
-    const {username} = useUser();
+    const {username, logout} = useUser();
+    const navigate = useNavigate()
+
+    function handleLogout() {
+        logout()
+        navigate("/")
+    }
     return(
-        <div>
+        <div className="flex justify-between">
             <h2>Hello, {username} 👋</h2>
+            <button className="cursor-pointer" onClick={handleLogout}>logout</button>
         </div>
     )
 }
--- src/contexts/userContext.tsx
+++ src/contexts/userContext.tsx
@@ -1,17 +1,34 @@
-import { createContext, useContext, useState, ReactNode } from "react";
+import { createContext, useContext, useState, ReactNode, useEffect } from "react";
 
 interface UserContextType {
   username: string;
   setUsername: (name: string) => void;
+  logout: () => void;
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
 export function UserProvider({ children }: { children: ReactNode }) {
-  const [username, setUsername] = useState("");
+  const [username, setUsernameState] = useState("");
+
+  useEffect(() => {
+    const savedUsername = localStorage.getItem("username");
+
+    savedUsername && setUsernameState(savedUsername);
+  }, [])
+
+  function setUsername(name: string) {
+    setUsernameState(name);
+    localStorage.setItem("username", name)
+  }
+
+  function logout() {
+    setUsernameState("")
+    localStorage.removeItem("username")
+  }
 
   return (
-    <UserContext.Provider value={{ username, setUsername }}>
+    <UserContext.Provider value={{ username, setUsername, logout }}>
       {children}
     </UserContext.Provider>
   );
